1e70fcb352fa156f976f8e3938d97e1b6b38c463
ronbout <ronbout@yahoo.com>
2019-07-30 20:58:50 -0500

    Add POST api for companies

--- code/companies.php
+++ code/companies.php
@@ -161,17 +161,27 @@ $app->post ( '/companies', function (Request $request, Response $response) {
 	$post_data = $request->getParsedBody ();
 	$data = array ();
 
-	$name = isset ( $post_data ['name'] ) ? filter_var ( $post_data ['name'], FILTER_SANITIZE_STRING ) : null;
-	$description = isset ( $post_data ['description'] ) ? filter_var ( $post_data ['description'], FILTER_SANITIZE_STRING ) : null;
-	$url = isset ( $post_data ['url'] ) ? filter_var ( $post_data ['url'], FILTER_SANITIZE_STRING ) : null;
+	$name = isset ( $post_data ['name'] ) ? filter_var ( $post_data ['name'], FILTER_SANITIZE_STRING ) : '';
+	$description = isset ( $post_data ['description'] ) ? filter_var ( $post_data ['description'], FILTER_SANITIZE_STRING ) : null;	
+	$company_phone = isset ( $post_data ['companyPhone'] ) ? filter_var ( $post_data ['companyPhone'], FILTER_SANITIZE_STRING ) : null;
+	$address_line1 = isset ( $post_data ['addressLine1'] ) ? filter_var ( $post_data ['addressLine1'], FILTER_SANITIZE_STRING ) : null;
+	$address_line2 = isset ( $post_data ['addressLine2'] ) ? filter_var ( $post_data ['addressLine2'], FILTER_SANITIZE_STRING ) : null;
+	$municipality = isset ( $post_data ['municipality'] ) ? filter_var ( $post_data ['municipality'], FILTER_SANITIZE_STRING ) : null;
+	$region = isset ( $post_data ['region'] ) ? filter_var ( $post_data ['region'], FILTER_SANITIZE_STRING ) : null;
+	$postal_code = isset ( $post_data ['postalCode'] ) ? filter_var ( $post_data ['postalCode'], FILTER_SANITIZE_STRING ) : null;
+	$country_code = isset ( $post_data ['countryCode'] ) ? filter_var ( $post_data ['countryCode'], FILTER_SANITIZE_STRING ) : null;
+	$email = isset ( $post_data ['email'] ) ? filter_var ( $post_data ['email'], FILTER_SANITIZE_STRING ) : null;
+	$website = isset ( $post_data ['website'] ) ? filter_var ( $post_data ['website'], FILTER_SANITIZE_STRING ) : null;
 
-	if (! $name) {
+	$contact_person_id = isset ( $post_data ['contactPersonId'] ) ? filter_var ( $post_data ['contactPersonId'], FILTER_SANITIZE_STRING ) : null;
+	
+	if ( !$name ) {
 		$data ['error'] = true;
-		$data ['message'] = 'Name is required.';
+		$data ['message'] = 'Company name is required.';
 		$newResponse = $response->withJson ( $data, 200, JSON_NUMERIC_CHECK );
 		return $newResponse;
 	}
-
+	
 	// login to the database. if unsuccessful, the return value is the
 	// Response to send back, otherwise the db connection;
 	$errCode = 0;
@@ -179,44 +189,38 @@ $app->post ( '/companies', function (Request $request, Response $response) {
 	if ($errCode) {
 		return $db;
 	}
+	
+	// create person item and get insert id
+	$query = 'INSERT INTO company
+							(name, description, companyPhone, contactPersonId, addressLine1, addressLine2, municipality, 
+							  region, postalCode, countryCode, email, website)
+							VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
-/* 	// need to make sure that skill name does not already exist as it must be unique
-	$stmt = $db->prepare ( 'SELECT * from skill WHERE name = ?' );
+	$insert_data = array($name, $description, $company_phone, $contact_person_id, $address_line1, $address_line2, 
+												$municipality, $region,	$postal_code, $country_code, $email, $website );
 
-	if (! $stmt->execute ( array ($name) )) {
-		$data ['error'] = true;
-		$data ['message'] = 'Database SQL Error Accessing Skill table: ' . $stmt->errorCode () . ' - ' . $stmt->errorInfo () [2];
-		$newResponse = $response->withJson ( $data, 500, JSON_NUMERIC_CHECK );
+	$response_data = pdo_exec( $request, $response, $db, $query, $insert_data, 'Creating Company', $errCode, false, false, false );
+	if ($errCode) {
+		return $response_data;
+	}
+
+
+	// get new company id to send back
+	if (! $company_id = $db->lastInsertId() ) {
+		// unknown insert error - should NOT get here
+		$return_data ['error'] = true;
+		$return_data ['errorCode'] = 45002; // unknown error
+		$return_data ['message'] = 'Unknown error creating Person';
+		$newResponse = $response->withJson ( $return_data, 500, JSON_NUMERIC_CHECK );
 		return $newResponse;
 	}
 
-	if (($stmt->rowCount())) {
-		$data ['error'] = true;
-		$data ['message'] = "Skill $name already exists";
-		$newResponse = $response->withJson ( $data, 200, JSON_NUMERIC_CHECK );
-		return $newResponse;
-	}
-
-	$stmt = $db->prepare ( 'INSERT INTO skill (name, description, url) VALUES ( ?,?,? )' ); 
-
-	if (! $stmt->execute ( array (
-			$name,
-			$description,
-			$url
-	) ) || ($stmt->rowCount () == 0)) {
-		$data ['error'] = true;
-		$data ['message'] = 'Database SQL Error Inserting Skill: ' . $stmt->errorCode () . ' - ' . $stmt->errorInfo () [2];
-		$newResponse = $response->withJson ( $data, 500, JSON_NUMERIC_CHECK );
-		return $newResponse;
-	}*/
-	// everything was fine. return success
-	$data ['id'] = $db->lastInsertId ();
-	$data ['name'] = $name;
-	$data ['description'] = $description;
-	$data ['url'] = $url;
+	// everything was fine. return success and the full data object
+	$data = $post_data;
+	$data ['id'] = $company_id;
 	// wrap it in data object
 	$data = array (
-			'data' => $data
+			'data' => $data 
 	);
 	$newResponse = $response->withJson ( $data, 201, JSON_NUMERIC_CHECK );
 	return $newResponse;
